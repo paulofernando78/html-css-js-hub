5a17d3f88a44ab56b13cea4cdfabb45efed71462
Paulo Fernando <paulofernando78@gmail.com>
2024-10-05 01:47:16 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ import { showHide } from "./lessons/button-show-hide.js";
 document.getElementById("toggle-button").addEventListener("click", () => {
   showHide("element");
 });
+
+import { modal, openModal, closeModal } from "./lessons/modal.js";
+
+openModal.addEventListener("click", () => {
+  modal.showModal()
+})
+
+closeModal.addEventListener("click", () => {
+  modal.close()
+})
