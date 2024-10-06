1afc291741bdd0b90ed61b56427228b456a62437
Paulo Fernando <paulofernando78@gmail.com>
2024-10-06 03:08:11 -0300

    wip

--- js/lessons/modal.js
+++ js/lessons/modal.js
@@ -1,4 +1,4 @@
-export const modal = document.getElementById("modal")
+export const modal = document.getElementById("modal")   
 export const openModal = document.getElementById("open-modal");
 export const closeModal = document.getElementById("close-modal");
 
